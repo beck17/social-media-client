4397a87fce2ae0ef3ace9eeba04813daa3612f2d
beck17 <bahcha1122001@gmail.com>
2025-03-10 18:43:32 +0300

    Replace the edit icons and delete the comments in the pop-up window.

--- src/components/ui/comment/CommentItem.tsx
+++ src/components/ui/comment/CommentItem.tsx
@@ -1,17 +1,10 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import Image from 'next/image'
 
 import { useAuth } from '@/hooks/useAuth'
 import { useDate } from '@/hooks/useDate'
 
-import { RemoveCommentForm } from '@/components/ui/edit-forms/comment-form/CommentRemoveForm'
-
-import Modal from '../modal/Modal'
-import CommentForm from '../edit-forms/comment-form/CommentForm'
-
-import trash from '@/assets/img/trash.svg'
-import edit from '@/assets/img/edit.svg'
-import noPhoto from '@/assets/img/no-photo.svg'
+import { CommentActions } from '@/components/ui/select/CommentActions'
 
 import { IComment } from '@/types/comment.interface'
 
@@ -29,30 +22,9 @@ const CommentItem: FC<Props> = ({
 																	postUserId,
 																	refetchComment,
 																}) => {
-
-	const [modalIsOpen, setIsOpen] = useState<boolean>(false)
-	const [removeModalIsOpen, setRemoveIsOpen] = useState<boolean>(false)
-
 	const { user } = useAuth()
-
 	return (
 		<div className={styles.comment}>
-			<Modal modalIsOpen={modalIsOpen} setIsOpen={setIsOpen}>
-				<CommentForm
-					refetch={refetchComment}
-					setIsOpen={setIsOpen}
-					commentId={comment._id}
-				/>
-			</Modal>
-
-			<Modal modalIsOpen={removeModalIsOpen} setIsOpen={setRemoveIsOpen}>
-				<RemoveCommentForm
-					refetch={refetchComment}
-					setIsOpen={setRemoveIsOpen}
-					commentId={comment._id}
-				/>
-			</Modal>
-
 			<Image
 				src={process.env.BASE_URL + `${comment.user.avatar}`}
 				alt='аватар'
@@ -63,53 +35,12 @@ const CommentItem: FC<Props> = ({
 			<div className={styles.info}>
 				<span>{`${comment.user.firstName} ${comment.user.lastName}`}</span>
 				<p>{comment.text}</p>
+				<span className={styles.date}>{useDate(comment.createdAt)}</span>
 			</div>
 
 			<div className={styles.detail}>
-				<span className={styles.date}>{useDate(comment.createdAt)}</span>
-				{postUserId === user?._id || comment.user._id === user?._id ? (
-					<div>
-						{comment.user._id === user?._id ? (
-							<Image
-								className={styles.img}
-								src={edit}
-								alt='Редактировать'
-								width={20}
-								height={20}
-								onClick={() => setIsOpen((prev) => !prev)}
-							/>
-						) : (
-							<Image
-								src={noPhoto}
-								alt='Ты не должен был это увидеть'
-								width={20}
-								height={20}
-							/>
-						)}
-						<Image
-							className={styles.img}
-							src={trash}
-							alt='Удалить'
-							width={20}
-							height={20}
-							onClick={() => setRemoveIsOpen((prev) => !prev)}
-						/>
-					</div>
-				) : (
-					<div>
-						<Image
-							src={noPhoto}
-							alt='Ты не должен был это увидеть'
-							width={20}
-							height={20}
-						/>
-						<Image
-							src={noPhoto}
-							alt='Ты не должен был это увидеть'
-							width={20}
-							height={20}
-						/>
-					</div>
+				{(postUserId === user?._id || comment.user._id === user?._id) && (
+					<CommentActions commentId={comment._id} refetch={refetchComment} />
 				)}
 			</div>
 		</div>
---
+++ src/components/ui/select/CommentActions.tsx
@@ -0,0 +1,62 @@
+import React, { FC, useState } from 'react'
+
+import { useOutsideClick } from '@/hooks/useOutsideClick'
+
+import { closePopupOpenModal } from '@/lib/close-popup-open-modal'
+
+import Modal from '@/components/ui/modal/Modal'
+import CommentForm from '@/components/ui/edit-forms/comment-form/CommentForm'
+import { RemoveCommentForm } from '@/components/ui/edit-forms/comment-form/CommentRemoveForm'
+
+import styles from '@/components/ui/select/Select.module.scss'
+
+
+interface Props {
+	commentId: string
+	refetch: () => void
+}
+
+export const CommentActions: FC<Props> = ({ commentId, refetch }) => {
+	const [isOpenPopup, setIsOpenPopup] = React.useState<boolean>(false)
+	const [modalEditIsOpen, setEditIsOpen] = useState<boolean>(false)
+	const [removeModalIsOpen, setRemoveIsOpen] = useState<boolean>(false)
+
+	const postActionRef = React.useRef<HTMLDivElement>(null)
+
+	useOutsideClick(postActionRef, () => setIsOpenPopup(false))
+
+	const handleUpdatePost = () => closePopupOpenModal(setIsOpenPopup, setEditIsOpen)
+	const handleRemovePost = () => closePopupOpenModal(setIsOpenPopup, setRemoveIsOpen)
+
+	return (
+		<div className={styles.sort} ref={postActionRef}>
+			<Modal modalIsOpen={modalEditIsOpen} setIsOpen={setEditIsOpen}>
+				<CommentForm
+					refetch={refetch}
+					setIsOpen={setEditIsOpen}
+					commentId={commentId}
+				/>
+			</Modal>
+
+			<Modal modalIsOpen={removeModalIsOpen} setIsOpen={setRemoveIsOpen}>
+				<RemoveCommentForm
+					refetch={refetch}
+					setIsOpen={setRemoveIsOpen}
+					commentId={commentId}
+				/>
+			</Modal>
+
+			<div className={styles.sort__label}>
+				<span onClick={() => setIsOpenPopup(!isOpenPopup)}>...</span>
+			</div>
+			{isOpenPopup && (
+				<div className={styles.sort__popup}>
+					<ul>
+						<li onClick={handleUpdatePost}>Редактировать</li>
+						<li onClick={handleRemovePost}>Удалить</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	)
+}
